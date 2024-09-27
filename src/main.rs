1dca20adb9b16b9b2fb0d05098386bae2dc9ee80
Esteban Forero <estebanmff@outlook.com>
2024-09-27 18:59:31 -0500

    Changed base ip to be the default

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,7 @@ async fn main() -> Result<(), Box<dyn error::Error>> {
         .nest("/user", user_service_router)
         .layer(CorsLayer::permissive());
 
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
         .await
         .unwrap();
 
